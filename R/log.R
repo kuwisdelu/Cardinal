698d35d43d043756d546a1dab1236588e4571454
kbemis <kbemis@purdue.edu>
2014-06-06 03:45:24 -0400

    added logging functions

---
+++ R/log.R
@@ -0,0 +1,103 @@
+
+#### Logging and opening and closing log files ####
+## ------------------------------------------------
+
+.log.append <- function(...) {
+	.Cardinal$log <- append(.Cardinal$log, paste(..., sep=""))
+}
+
+.log.flush <- function(pkgname="Cardinal") {
+	tryCatch({
+		if ( length(.Cardinal$log) != 0 ) {
+			filename <- paste(pkgname, "log", sep=".")
+			filepath <- file.path(system.file(package=pkgname), filename)
+			sink(filepath, append=TRUE)
+			cat(paste(Sys.time()), "\n\n")
+			for ( m in .Cardinal$log ) {
+				cat(m, "\n\n")
+			}
+			.Cardinal$log <- list()
+			sink()
+		}
+		TRUE
+	}, error=function(e) FALSE)
+}
+
+.log <- function(..., progress=c("none", "start", "stop", "increment"), min=0, max=1) {
+	.log.append(...)
+	progress <- match.arg(progress)
+	if ( progress == "none" ) {
+		for ( f in .Cardinal$message ) {
+			f(...)
+		}
+	} else if ( progress == "start" ) {
+		.Cardinal$progress$min <- min
+		.Cardinal$progress$max <- max
+		for ( f in .Cardinal$progress$start ) {
+			f(..., min=min, max=max)
+		}
+	} else if ( progress == "increment" ) {
+		.Cardinal$progress$i <- .Cardinal$progress$i + 1
+		for ( f in .Cardinal$progress$increment ) {
+			f()
+		}
+	} else if ( progress == "stop" ) {
+		for ( f in .Cardinal$progress$stop ) {
+			f(...)
+		}
+	}
+}
+
+#### Console messages and progress bars ####
+## -----------------------------------------
+
+.message <- function(...) {
+	if ( getOption("Cardinal.verbose") ) {
+		message(...)
+		flush.console()
+	}
+}
+
+.progress.start <- function(..., min=0, max=1) {
+	.message(...)
+	if ( getOption("Cardinal.progress") ) {
+		.Cardinal$progress$bar <- txtProgressBar(min=min, max=max, style=3)
+	}
+	.Cardinal$time$start <- proc.time()
+}
+
+.progress.increment <- function() {
+	if ( getOption("Cardinal.progress") ) {
+		setTxtProgressBar(.Cardinal$progress$bar, value=.Cardinal$progress$i)
+	}
+}
+
+.progress.stop <- function(...) {
+	.Cardinal$time$stop <- proc.time()
+	if ( getOption("Cardinal.progress") ) {
+		close(.Cardinal$progress$bar)
+	}
+	.message(...)
+}
+
+#### Generate entry for history log ####
+## -------------------------------------
+
+.history <- function(which=-2) {
+	error <- paste("error in", deparse(match.call()))
+	history <- tryCatch({
+		call <- sys.call(which)
+		history <- paste("Call:", deparse(call))
+		if ( length(call) != 1 ) {
+			args <- sapply(as.list(call)[-1], function(x) {
+				tryCatch({
+					class(eval(x, envir=sys.frame(which)))
+				}, error=function(e) "unknown")
+			})
+			history <- append(history, paste(names(args), args, sep=": "))
+		}
+		paste(history, collapse="\n")
+	}, error=function(e) error)
+	.log.append(history)
+	history
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,20 +1,32 @@
 
 #### set up the Cardinal environment ####
 
+.Cardinal <- list2env(list(
+	log=list(),
+	message=list(.message),
+	progress=list(
+		bar=NULL,
+		min=numeric(),
+		max=numeric(),
+		i=numeric(),
+		start=list(.progress.start),
+		increment=list(.progress.increment),
+		stop=list(.progress.stop)),
+	time=list(
+		start=NULL,
+		stop=NULL)))
+
 .onLoad <- function(libname, pkgname) {
-	.CardinalState <- new.env()
-	.verboseState <- new.env()
-	options(Cardinal.verbose.output=FALSE)
-	options(Cardinal.track.progress=FALSE)
+	options(Cardinal.verbose=TRUE)
+	options(Cardinal.progress=FALSE)
 }
 
 .onAttach <- function(libname, pkgname) {
-	msg0 <- paste("Welcome to Cardinal (version ", packageVersion("Cardinal"), ")\n", sep="")
+	msg0 <- paste("Welcome to Cardinal (version ",
+		packageVersion("Cardinal"), ")\n\n", sep="")
 	msg1 <- "To get started, view the introductory
 	 		vignettes with 'browseVignettes(\"Cardinal\")'."
 	msg1 <- strwrap(paste(msg1, collapse=""), exdent=4, indent=4)
 	packageStartupMessage(msg0, paste(msg1, collapse="\n"), "\n")
-	# packageStartupMessage(paste(msg0, "\n"))
 	addVigs2WinMenu("Cardinal") 
 }
-
