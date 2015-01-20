951fec04b9f5416f0ca4d23ad5b8daa1dfffdd2e
kbemis <kbemis@purdue.edu>
2015-01-20 07:53:36 -0500

    adjust nipals unit tests; version bump for BioC

--- inst/tests/test_nipals.R
+++ inst/tests/test_nipals.R
@@ -10,9 +10,9 @@ test_that("NIPALS - PCA", {
 	svd <- prcomp(scale(x, scale=FALSE))
 	pca <- nipals.PCA(scale(x, scale=FALSE), ncomp=10)
 
-	expect_equal(abs(svd$rotation), abs(pca$loadings), tolerance=0.2)
+	expect_equal(abs(svd$rotation), abs(pca$loadings), tolerance=0.11)
 
-	expect_equal(abs(svd$x), abs(pca$scores), tolerance=0.2)
+	expect_equal(abs(svd$x), abs(pca$scores), tolerance=0.11)
 
 })
 
@@ -33,9 +33,9 @@ test_that("NIPALS - PLS", {
 	p0.1 <- c(-0.45, -0.89)
 	b0 <- c(-1, -2)
 
-	expect_equal(w0.1, pls0$weights[,1], tolerance=0.01)
-	expect_equal(p0.1, pls0$loadings[,1], tolerance=0.01)
-	expect_equal(b0, pls0$coefficients[,1], tolerance=0.01)
+	expect_equal(w0.1, pls0$weights[,1], tolerance=0.1)
+	expect_equal(p0.1, pls0$loadings[,1], tolerance=0.1)
+	expect_equal(b0, pls0$coefficients[,1], tolerance=0.1)
 
 	x1 <- rbind(c(-2.18, -2.18),
 				c(1.84, -0.16),
@@ -48,9 +48,9 @@ test_that("NIPALS - PLS", {
 	p1.1 <- c(-0.69, -0.77)
 	b1 <- c(0.08, -1.08)
 
-	expect_equal(w1.1, pls1$weights[,1], tolerance=0.01)
-	expect_equal(p1.1, pls1$loadings[,1], tolerance=0.01)
-	expect_equal(b1, pls1$coefficients[,1], tolerance=0.01)
+	expect_equal(w1.1, pls1$weights[,1], tolerance=0.1)
+	expect_equal(p1.1, pls1$loadings[,1], tolerance=0.1)
+	expect_equal(b1, pls1$coefficients[,1], tolerance=0.1)
 
 })
 
@@ -71,9 +71,9 @@ test_that("NIPALS - OPLS", {
 	po.1 <- c(-1.16, -0.09)
 	to.1 <- c(0.97, -1.71, 1.11, -0.37)
 
-	expect_equal(wo.1, opls$Oweights[,1], tolerance=0.01)
-	expect_equal(po.1, opls$Oloadings[,1], tolerance=0.01)
-	expect_equal(to.1, opls$Oscores[,1], tolerance=0.01)
+	expect_equal(wo.1, opls$Oweights[,1], tolerance=0.1)
+	expect_equal(po.1, opls$Oloadings[,1], tolerance=0.1)
+	expect_equal(to.1, opls$Oscores[,1], tolerance=0.1)
 
 	pls <- nipals.PLS(opls$Xnew, y, ncomp=1)
 
@@ -81,9 +81,9 @@ test_that("NIPALS - OPLS", {
 	p.1 <- c(-0.45, -0.89)
 	b <- c(-0.41, -0.82)
 
-	expect_equal(w.1, pls$weights[,1], tolerance=0.01)
-	expect_equal(p.1, pls$loadings[,1], tolerance=0.01)
-	expect_equal(b, pls$coefficients[,1], tolerance=0.01)
+	expect_equal(w.1, pls$weights[,1], tolerance=0.1)
+	expect_equal(p.1, pls$loadings[,1], tolerance=0.1)
+	expect_equal(b, pls$coefficients[,1], tolerance=0.1)
 
 })
 
