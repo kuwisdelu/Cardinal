2e25ba4267ff702644cea722b62e3fb955509d75
k.bemis <k.bemis@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-25 08:23:48 +0000

    v1.4.1 fixed SImageData subset bug
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Cardinal@117832 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-SImageData.R
+++ R/methods-SImageData.R
@@ -202,9 +202,9 @@ setMethod("[", "SImageData", function(x, i, j, ..., drop) {
 		if ( !missing(j) ) args[[2]] <- j
 		if ( nargs > 2 ) {
 			dots <- match.call(expand.dots=FALSE)$...
-			nonmissing <- !sapply(dots, is.symbol)
+			nonmissing <- sapply(dots, nchar) > 0 # changed from !is.symbol
 			if ( sum(nonmissing) > 0 )
-				args[c(FALSE,FALSE,nonmissing)] <- dots[nonmissing]
+				args[c(FALSE,FALSE,nonmissing)] <- lapply(dots[nonmissing], eval)
 		}
 		inds <- do.call("[", c(list(x@positionArray), args[-1], drop=FALSE))
 		cube <- matrix(NA, nrow=length(args[[1]]), ncol=length(inds))
