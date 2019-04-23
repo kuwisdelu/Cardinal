384308b9a9c8a2711c93ac6edb6eb4ad9c94ec52
kuwisdelu <k.bemis@northeastern.edu>
2019-04-23 17:06:07 -0400

    updated pre-processing tests

--- tests/testthat/test2-processing.R
+++ tests/testthat/test2-processing.R
@@ -7,9 +7,11 @@ options(Cardinal.progress=interactive(), Cardinal.verbose=interactive())
 
 register(SerialParam())
 
-test_that("Cardinal >=2 delayed processing", {
+set.seed(1)
+data <- simulateImage(preset=1, dim=c(10,10), baseline=1)
+data_c <- data[,pData(data)$circle]
 
-	data <- simulateImage(preset=1, dim=c(10,10))
+test_that("Cardinal >=2 delayed processing", {
 
 	tmp <- process(data, function(s) log2(abs(s)))
 
@@ -24,3 +26,36 @@ test_that("Cardinal >=2 delayed processing", {
 	expect_true(validObject(tmp3))
 
 })
+
+test_that("pre-processing 2", {
+
+	tmp <- normalize(data_c, method="rms") %>% process()
+
+	expect_true(validObject(tmp))
+
+	tmp2 <- smoothSignal(tmp, method="gaussian") %>% process()
+
+	expect_true(validObject(tmp2))
+
+	tmp3 <- reduceBaseline(tmp2, method="locmin") %>% process()
+
+	expect_true(validObject(tmp3))
+
+	tmp4 <- peakPick(tmp3, method="simple") %>% process()
+
+	expect_true(validObject(tmp4))
+
+	tmp5 <- peakAlign(tmp4, tolerance=400, units="ppm") %>% process()
+
+	expect_true(validObject(tmp5))
+
+	tmp6 <- peakFilter(tmp5, freq.min=0.1) %>% process()
+
+	expect_true(validObject(tmp6))
+
+	data_p <- peakBin(tmp3, ref=mz(tmp6), tolerance=1, units="mz") %>% process()
+
+	data_b <- mzBin(tmp3, to=800, resolution=400, units="ppm") %>% process()
+
+})
+
