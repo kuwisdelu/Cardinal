964f41fa78b4413df6603482c89866582a4fdcb6
kuwisdelu <kuwisdelu@northeastern.edu>
2019-04-26 15:25:28 -0400

    v2.1.27 add mzFilter as alias for peakFilter w diff defaults

--- R/process2-peakFilter.R
+++ R/process2-peakFilter.R
@@ -3,34 +3,53 @@
 ## --------------------------------------------
 
 setMethod("peakFilter", "MSImagingExperiment",
-	function(object, freq.min = 0.01, ...)
+	function(object, ..., freq.min = 0.01, thresh.max = 0)
 	{
 		expr <- eval(substitute(alist(...)))
-		postfun <- peakFilter_postfun(expr, freq.min)
+		postfun <- peakFilter_postfun(expr, freq.min, thresh.max)
 		object <- process(object, label="peakFilter",
 			kind="global", postfun=postfun,
 			delay=getOption("Cardinal.delay"))
 		object
 	})
 
-peakFilter_postfun <- function(expr, freq.min) {
+setMethod("mzFilter", "MSImagingExperiment",
+	function(object, ..., freq.min = 0, thresh.max = 0.01)
+	{
+		peakFilter(object, freq.min=freq.min, thresh.max=thresh.max, ...)
+	})
+
+peakFilter_postfun <- function(expr, freq.min, thresh.max) {
 	fun <- function(object, ..., BPPARAM) {
-		summary1 <- summarize(object, .stat=c(count="sum", freq="mean"),
-			.tform=function(x) x > 0, .by="feature", BPPARAM=BPPARAM)
-		keep <- summary1$freq > freq.min
-		if ( length(expr) > 1L ) {
+		if ( freq.min > 0 ) {
+			summary1 <- summarize(object, .stat=c(count="sum", freq="mean"),
+				.tform=function(x) x > 0, .by="feature", BPPARAM=BPPARAM)
+			.message("applying freq.min = ", freq.min)
+			keep <- summary1$freq > freq.min
+		} else {
+			keep <- rep_len(TRUE, nrow(object))
+		}
+		if ( length(expr) > 0L || thresh.max > 0 ) {
 			summary2 <- summarize(object,
 				.stat=c("min", "max", "mean", "sum", "sd", "var"),
 				.by="feature", BPPARAM=BPPARAM)
-			summary1 <- cbind(summary1, summary2)
-			e <- as.env(summary1)
-			rules <- lapply(expr, function(a) {
-				rule <- eval(a, envir=e)
-				if ( !is.logical(rule) )
-					.stop("filter rules must be logical vectors")
-				rule
-			})
-			keep <- keep & rowSums(do.call(cbind, rules)) != 0
+			if ( !all(keep) )
+				summary2 <- cbind(summary1, summary2)
+			if ( thresh.max > 0 ) {
+				.message("applying thresh.max = ", thresh.max)
+				keep <- keep & (summary2$mean > thresh.max * max(summary2$mean))
+			}
+			if ( length(expr) > 0L ) {
+				e <- as.env(summary2)
+				rules <- lapply(expr, function(a) {
+					.message("applying rule = ", deparse(a))
+					rule <- eval(a, envir=e)
+					if ( !is.logical(rule) )
+						.stop("filter rules must be logical vectors")
+					rule
+				})
+				keep <- keep & rowSums(do.call(cbind, rules)) != 0
+			}
 		}
 		.message("removing ", sum(!keep), " peaks; ",
 			"keeping ", sum(keep), " peaks")
