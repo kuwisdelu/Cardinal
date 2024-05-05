4eebf1d5e8bfea3a9f359f992cc1cd0f5b234807
Kylie Bemis <k.bemis@northeastern.edu>
2024-05-05 12:55:38 -0400

    update bin() arguments

--- tests/testthat/test-process-bin.R
+++ tests/testthat/test-process-bin.R
@@ -22,7 +22,12 @@ test_that("process bin - SpectralImagingArrays", {
 
 	expect_equal(s4, s3)
 	expect_equal(range(fData(s5)$index), c(100, 500))
-	expect_equal(estres(fData(s5)$index), c(absolute=1))
+	expect_equal(matter::estres(fData(s5)$index), c(absolute=1))
+
+	s6 <- bin(s, ref=c(100, 500), tolerance=1, units="absolute")
+
+	expect_equal(fData(s6)$index, c(100, 500))
+	expect_equivalent(tolerance(spectra(s6)), 1)
 
 })
 
@@ -45,7 +50,12 @@ test_that("process bin - SpectralImagingExperiment", {
 
 	expect_equal(s4, s3)
 	expect_equal(range(fData(s5)$index), c(100, 500))
-	expect_equal(estres(fData(s5)$index), c(absolute=1))
+	expect_equal(matter::estres(fData(s5)$index), c(absolute=1))
+
+	s6 <- bin(s, ref=c(100, 500), tolerance=1, units="absolute")
+
+	expect_equal(fData(s6)$index, c(100, 500))
+	expect_equivalent(tolerance(spectra(s6)), 1)
 	
 })
 
@@ -56,11 +66,20 @@ test_that("process bin - MSImagingArrays", {
 
 	ms2 <- bin(ms, units="ppm")
 	ms3 <- bin(ms, units="mz")
+	ms4 <- bin(ms, resolution=1, units="mz")
+	ms5 <- bin(ms, mass.range=c(200, 600), resolution=1, units="mz")
+	ms6 <- bin(ms, ref=152.9, tolerance=50, units="ppm")
 
 	expect_is(ms2, "MSImagingExperiment")
 	expect_is(ms3, "MSImagingExperiment")
 	expect_is(spectra(ms2), "sparse_mat")
 	expect_is(spectra(ms3), "sparse_mat")
+	expect_equal(mz(ms4), 100:800)
+	expect_equal(mz(ms5), 200:600)
+	expect_equal(mz(ms6), 152.9)
+	expect_equivalent(tolerance(spectra(ms6)), 5e-5)
+
+	expect_error(bin(ms, mass.range=c(200, 600)))
 
 })
 
@@ -71,10 +90,19 @@ test_that("process bin - MSImagingExperiment", {
 
 	ms2 <- bin(ms, units="ppm")
 	ms3 <- bin(ms, units="mz")
+	ms4 <- bin(ms, resolution=1, units="mz")
+	ms5 <- bin(ms, mass.range=c(200, 600), resolution=1, units="mz")
+	ms6 <- bin(ms, ref=152.9, tolerance=50, units="ppm")
 
 	expect_is(ms2, "MSImagingExperiment")
 	expect_is(ms3, "MSImagingExperiment")
 	expect_is(spectra(ms2), "sparse_mat")
 	expect_is(spectra(ms3), "sparse_mat")
+	expect_equal(mz(ms4), 100:800)
+	expect_equal(mz(ms5), 200:600)
+	expect_equal(mz(ms6), 152.9)
+	expect_equivalent(tolerance(spectra(ms6)), 5e-5)
+
+	expect_error(bin(ms, mass.range=c(200, 600)))
 
 })
