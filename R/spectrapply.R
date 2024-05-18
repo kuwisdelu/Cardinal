8f6e1752a5d5c75f299bd3b2561d65be34dea173
kuwisdelu <k.bemis@northeastern.edu>
2024-05-18 16:04:07 -0600

    update spectrapply()

--- R/spectrapply.R
+++ R/spectrapply.R
@@ -28,24 +28,38 @@ setMethod("spectrapply", "SpectralImagingExperiment",
 	{
 		if ( length(processingData(object)) > 0L )
 			warning("processing steps will be ignored by spectrapply()")
-		xnm <- spectra
-		tnm <- index
-		spectra <- spectra(object, xnm)
-		if ( is.null(tnm) ) {
-			tnm <- "index"
+		nindex <- max(1L, length(index))
+		snm <- spectra
+		inm <- index
+		spectra <- spectra(object, snm)
+		if ( is.null(inm) ) {
+			inm <- "index"
 			index <- seq_len(nrow(object))
 		} else {
-			index <- featureData(object)[[tnm]]
+			index <- featureData(object)[[inm[1L]]]
 			if ( is.null(index) )
-				stop("index ", sQuote(tnm), " not found")
+				stop("index ", sQuote(inm[1L]), " not found")
+			if ( nindex > 1L ) {
+				index2 <- featureData(object)[[inm[2L]]]
+				if ( is.null(index2) )
+					stop("index ", sQuote(inm[2L]), " not found")
+			}
 		}
 		if ( ...length() > 0L && is.null(...names()) )
 			stop("spectrapply() arguments passed via ... must be named")
-		FUN2 <- function(x, ...) FUN(x, index, ...)
-		chunkApply(spectra, FUN=FUN2, MARGIN=2L, ...,
-			simplify=simplify, outpath=outpath,
-			nchunks=nchunks, verbose=verbose,
-			BPPARAM=BPPARAM)
+		if ( nindex == 1L ) {
+			chunkApply(spectra, 2L, FUN, index, ...,
+				simplify=simplify, outpath=outpath,
+				nchunks=nchunks, verbose=verbose,
+				BPPARAM=BPPARAM)
+		} else if ( nindex == 2L ) {
+			chunkApply(spectra, 2L, FUN, index, index2, ...,
+				simplify=simplify, outpath=outpath,
+				nchunks=nchunks, verbose=verbose,
+				BPPARAM=BPPARAM)
+		} else {
+			stop("more than 2 'index' arrays not allowed")
+		}
 	})
 
 setMethod("spectrapply", "SpectralImagingArrays",
@@ -58,23 +72,38 @@ setMethod("spectrapply", "SpectralImagingArrays",
 	{
 		if ( length(processingData(object)) > 0L )
 			warning("processing steps will be ignored by spectrapply()")
-		xnm <- spectra
-		tnm <- index
-		spectra <- spectra(object, xnm)
-		if ( is.null(tnm) ) {
-			tnm <- "index"
+		nindex <- max(1L, length(index))
+		snm <- spectra
+		inm <- index
+		spectra <- spectra(object, snm)
+		if ( is.null(inm) ) {
+			inm <- "index"
 			index <- lapply(lengths(spectra), seq_len)
 		} else {
-			index <- spectra(object, tnm)
+			index <- spectra(object, inm[1L])
 			if ( is.null(index) )
-				stop("index ", sQuote(tnm), " not found")
+				stop("index ", sQuote(inm[1L]), " not found")
+			if ( nindex > 1L ) {
+				index2 <- spectra(object, inm[2L])
+				if ( is.null(index2) )
+					stop("index ", sQuote(inm[2L]), " not found")
+			}
 		}
 		if ( ...length() > 0L && is.null(...names()) )
 			stop("spectrapply() arguments passed via ... must be named")
 		args <- list(...)
-		chunkMapply(FUN, spectra, index, MoreArgs=args,
-			simplify=simplify, outpath=outpath,
-			nchunks=nchunks, verbose=verbose,
-			BPPARAM=BPPARAM)
+		if ( nindex == 1L ) {
+			chunkMapply(FUN, spectra, index, MoreArgs=args,
+				simplify=simplify, outpath=outpath,
+				nchunks=nchunks, verbose=verbose,
+				BPPARAM=BPPARAM)
+		} else if ( nindex == 2L ) {
+			chunkMapply(FUN, spectra, index, index2, MoreArgs=args,
+				simplify=simplify, outpath=outpath,
+				nchunks=nchunks, verbose=verbose,
+				BPPARAM=BPPARAM)
+		} else {
+			stop("more than 2 'index' arrays not allowed")
+		}
 	})
 
--- tests/testthat/test-spectrapply.R
+++ tests/testthat/test-spectrapply.R
@@ -3,7 +3,85 @@ require(Cardinal)
 
 context("spectrapply")
 
-test_that("spectrapply", {
+test_that("spectrapply - SpectralImagingArrays", {
+
+	set.seed(1, kind="default")
+	x <- replicate(9, rlnorm(100), simplify=FALSE)
+	t1 <- replicate(9, sort(runif(100)), simplify=FALSE)
+	t2 <- replicate(9, sort(runif(100)), simplify=FALSE)
+
+	sa <- SpectralImagingArrays(
+		spectraData=list(x=x, t1=t1, t2=t2),
+		pixelData=PositionDataFrame(expand.grid(x=1:3, y=1:3)))
+
+	xout <- spectrapply(sa,
+		FUN=function(x, t1, t2, ...) x,
+		spectra="x", index=c("t1", "t2"),
+		simplify=FALSE)
+	
+	t1out <- spectrapply(sa,
+		FUN=function(x, t1, t2, ...) t1,
+		spectra="x", index=c("t1", "t2"),
+		simplify=FALSE)
+
+	t2out <- spectrapply(sa,
+		FUN=function(x, t1, t2, ...) t2,
+		spectra="x", index=c("t1", "t2"),
+		simplify=FALSE)
+
+	expect_equal(spectra(sa, "x"), xout)
+	expect_equal(spectra(sa, "t1"), t1out)
+	expect_equal(spectra(sa, "t2"), t2out)
+
+})
+
+test_that("spectrapply - SpectralImagingExperiment", {
+
+	set.seed(1, kind="default")
+	x <- replicate(9, rlnorm(100), simplify=TRUE)
+	t1 <- sort(runif(100))
+	t2 <- sort(runif(100))
+
+	se <- SpectralImagingExperiment(
+		spectraData=list(x=x),
+		featureData=DataFrame(t1=t1, t2=t2),
+		pixelData=PositionDataFrame(expand.grid(x=1:3, y=1:3)))
+
+	xout <- spectrapply(se,
+		FUN=function(x, t1, t2, ...) x,
+		spectra="x", index=c("t1", "t2"),
+		simplify=TRUE)
+	
+	t1out <- spectrapply(se,
+		FUN=function(x, t1, t2, ...) t1,
+		spectra="x", index=c("t1", "t2"),
+		simplify=TRUE)
+
+	t2out <- spectrapply(se,
+		FUN=function(x, t1, t2, ...) t2,
+		spectra="x", index=c("t1", "t2"),
+		simplify=TRUE)
+
+	expect_equal(spectra(se, "x"), xout)
+	expect_equal(featureData(se)$t1, t1out[,1L])
+	expect_equal(featureData(se)$t2, t2out[,2L])
+
+})
+
+test_that("spectrapply - MSImagingArrays", {
+
+	path <- CardinalIO::exampleImzMLFile("processed")
+	msa <- readImzML(path, memory=TRUE)
+
+	xout <- spectrapply(msa, function(x, t, ...) x, simplify=FALSE)
+	mzout <- spectrapply(msa, function(x, t, ...) t, simplify=FALSE)
+
+	expect_equal(intensity(msa), xout)
+	expect_equal(mz(msa), mzout)
+
+})
+
+test_that("spectrapply - MSImagingExperiment", {
 
 	path <- CardinalIO::exampleImzMLFile("continuous")
 	mse <- readImzML(path, memory=TRUE)
@@ -14,14 +92,4 @@ test_that("spectrapply", {
 	expect_equal(spectra(mse), xout)
 	expect_equal(mz(mse), mzout[,1L])
 
-	path2 <- CardinalIO::exampleImzMLFile("processed")
-	mse2 <- readImzML(path2, memory=TRUE)
-
-	xout2 <- spectrapply(mse2, function(x, t, ...) x, simplify=FALSE)
-	mzout2 <- spectrapply(mse2, function(x, t, ...) t, simplify=FALSE)
-
-	expect_equal(intensity(mse2), xout2)
-	expect_equal(mz(mse2), mzout2)
-
 })
-
