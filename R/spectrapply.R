a06b56eca796d15fd79a1de6f64dbc7a7ee0c6ce
Kylie Bemis <k.bemis@northeastern.edu>
2024-09-02 18:06:20 -0400

    update spectrapply()

--- R/spectrapply.R
+++ R/spectrapply.R
@@ -11,8 +11,8 @@ setMethod("spectrapply", "SpectralImagingExperiment",
 	{
 		if ( length(processingData(object)) > 0L )
 			.Warn("processing steps will be ignored by spectrapply()")
-		if ( length(index) > 3L )
-			.Error("more than 3 'index' arrays not allowed ",
+		if ( length(index) > 1L )
+			.Error("more than 1 'index' array not allowed ",
 				"for class ", sQuote(class(object)[1L]))
 		nindex <- max(0L, length(index))
 		snm <- spectra
@@ -22,16 +22,6 @@ setMethod("spectrapply", "SpectralImagingExperiment",
 			index <- featureData(object)[[inm[1L]]]
 			if ( is.null(index) )
 				.Error("index ", sQuote(inm[1L]), " not found")
-			if ( nindex >= 2L ) {
-				index2 <- featureData(object)[[inm[2L]]]
-				if ( is.null(index2) )
-					.Error("index ", sQuote(inm[2L]), " not found")
-			}
-			if ( nindex >= 3L ) {
-				index3 <- featureData(object)[[inm[3L]]]
-				if ( is.null(index2) )
-					.Error("index ", sQuote(inm[3L]), " not found")
-			}
 		}
 		if ( ...length() > 0L && is.null(...names()) )
 			.Error("spectrapply() arguments passed via ... must be named")
@@ -40,18 +30,8 @@ setMethod("spectrapply", "SpectralImagingExperiment",
 				simplify=simplify, outpath=outpath,
 				verbose=verbose, chunkopts=chunkopts,
 				BPPARAM=BPPARAM)
-		} else if ( nindex == 1L ) {
+		} else {
 			chunkApply(spectra, 2L, FUN, index, ...,
-				simplify=simplify, outpath=outpath,
-				verbose=verbose, chunkopts=chunkopts,
-				BPPARAM=BPPARAM)
-		} else if ( nindex == 2L ) {
-			chunkApply(spectra, 2L, FUN, index, index2, ...,
-				simplify=simplify, outpath=outpath,
-				verbose=verbose, chunkopts=chunkopts,
-				BPPARAM=BPPARAM)
-		} else if ( nindex == 3L ) {
-			chunkApply(spectra, 2L, FUN, index, index2, index3, ...,
 				simplify=simplify, outpath=outpath,
 				verbose=verbose, chunkopts=chunkopts,
 				BPPARAM=BPPARAM)
