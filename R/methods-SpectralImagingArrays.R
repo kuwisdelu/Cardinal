193dcf023b62c4879a3a3ad16cd057b19a5808ef
Kylie Bemis <k.bemis@northeastern.edu>
2026-02-03 13:03:52 -0500

    separated files for SpectralImagingArrays and SpectralImagingData methods

---
+++ R/methods-SpectralImagingArrays.R
@@ -0,0 +1,230 @@
+
+#### SpectralImagingArrays ####
+## ----------------------------
+
+# Class for a list of (unprocessed) spectra
+# _without_ any aligned feature information
+
+.valid_SpectralImagingArrays <- function(object)
+{
+	errors <- NULL
+	if ( length(object@spectraData) > 0L )
+	{
+		nr_spectra <- nrow(object@spectraData)
+		nr_pixelData <- nrow(object@elementMetadata)
+		if ( length(dim(object@spectraData)) != 2L )
+			errors <- c(errors, paste0("number of dimensions of spectraData [",
+				length(dim(object@spectraData)), "] must be 2"))
+		if ( nr_spectra != nr_pixelData )
+			errors <- c(errors, paste0("number of rows in spectraData [",
+				nr_spectra, "] must match number of rows in pixelData [",
+				nr_pixelData, "]"))
+	}
+	if ( is.null(errors) ) TRUE else errors
+}
+
+setValidity("SpectralImagingArrays", .valid_SpectralImagingArrays)
+
+SpectralImagingArrays <- function(spectraData = SimpleList(),
+	pixelData = PositionDataFrame(), metadata = list())
+{
+	spectraData <- SpectraArrays(spectraData)
+	if ( length(spectraData) != 0L )
+	{
+		spectra <- spectraData[[1L]]
+		if ( missing(pixelData) )
+		{
+			colnames <- colnames(spectra)
+			coord <- expand.grid(x=seq_len(length(spectra)), y=1L)
+			pixelData <- PositionDataFrame(coord=coord, row.names=colnames)
+		}
+	}
+	new("SpectralImagingArrays", spectraData=spectraData,
+		elementMetadata=pixelData, metadata=metadata,
+		processing=list())
+}
+
+.paste_head_tail <- function(x, n = 6L, collapse = ", ")
+{
+	paste0(.select_head_tail(x, n), collapse=collapse)
+}
+
+.print_queued_processing <- function(object)
+{
+	if ( length(processingData(object)) > 0L )
+	{
+		cat(sprintf("processingData(%d): %s\n", length(processingData(object)),
+			.paste_head_tail(names(processingData(object)))))
+		cat("NOTE: use process() to execute queued processing steps\n")
+		cat("NOTE: use reset() to remove queued processing steps\n")
+		cat("NOTE: use plot() to preview queued processing steps\n")
+	}
+}
+
+setMethod("show", "SpectralImagingArrays",
+	function(object) {
+		# dimensions
+		label <- if(length(object) != 1L) "spectra" else "spectrum"
+		cat(class(object), "with", length(object), label, "\n")
+		# spectraData()
+		cat(sprintf("spectraData(%d): %s\n", length(spectraData(object)),
+			.paste_head_tail(names(spectraData(object)))))
+		# pixelData()
+		cat(sprintf("pixelData(%d): %s\n", length(pixelData(object)),
+			.paste_head_tail(names(pixelData(object)))))
+		# coord()
+		if ( length(object) > 0L )
+		{
+			lims <- vapply(coord(object), range, numeric(2L))
+			lims <- paste0(coordNames(object), " = ", lims[1L,], "...", lims[2L,])
+			cat(sprintf("coord(%d): %s\n", length(coordNames(object)),
+				.paste_head_tail(lims)))
+		}
+		# runNames()
+		cat(sprintf("runNames(%d): %s\n", length(runNames(object)),
+			.paste_head_tail(runNames(object))))
+		# processingData()
+		.print_queued_processing(object)
+		# metadata()
+		if ( length(metadata(object)) > 0L )
+		{
+			cat(sprintf("metadata(%d): %s\n", length(metadata(object)),
+				.paste_head_tail(names(metadata(object)))))
+		}
+	})
+
+# find spectra by position
+setMethod("pixels", "SpectralImagingArrays",
+	function(object, ..., coord, run, tolerance = NA,
+		env = NULL)
+	{
+		if ( is.null(env) )
+			env <- parent.frame(2)
+		i <- callNextMethod(object, ..., env=env)
+		if ( !missing(coord) || !missing(run) ) {
+			pos <- .find_positions(object, coord, run, tolerance)
+			i <- intersect(i, pos)
+		}
+		setNames(i, pixelNames(object)[i])
+	})
+
+.find_positions <- function(object, coord, run, tol = NA)
+{
+	index <- seq_len(nrow(pixelData(object)))
+	if ( !missing(coord) && !is.null(coord) ) {
+		if ( missing(run) )
+			run <- NULL
+		coord <- as.list(coord)
+		if ( length(coord) != ncol(coord(object)) )
+			.Error("length of coord [", length(coord), "] does not ",
+				"match object coord [", ncol(coord(object)), "]")
+		if ( is.null(names(coord)) ) {
+			names(coord) <- coordNames(object)
+		} else {
+			coord <- coord[coordNames(object)]
+		}
+		coord <- as.data.frame(coord)
+		if ( is.na(tol) )
+			tol <- 0.5 * vapply(coord(object), estres, numeric(1L))
+		i_coord <- kdsearch(coord, coord(object), tol=tol)
+		if ( any(lengths(i_coord) != 1L) )
+		{
+			for ( j in which(lengths(i_coord) != 1L) )
+			{
+				badmatch <- paste0(names(coord), " = ",
+					unlist(coord[j,]), collapse=", ")
+				if ( length(i_coord[[j]]) > 1L ) {
+					i_run <- run(object)[i_coord[[j]]]
+					dup <- !setequal(unique(i_run), i_run)
+					if ( dup )
+						.Warn("multiple matches for coord ", badmatch)
+				} else {
+					k <- as.vector(knnsearch(coord[j,], coord(object), k=1L))
+					k_coord <- as.list(coord(object)[k,])
+					nearmatch <- paste0(coordNames(object), " = ",
+						unlist(k_coord), collapse=", ")
+					.Warn("no match for coord ", badmatch, "; ",
+						"nearest is ", nearmatch)
+				}
+			}
+		}
+		i_coord <- unique(unlist(i_coord))
+		index <- intersect(i_coord, index)
+	}
+	if ( !missing(run) && !is.null(run) ) {
+		if ( missing(coord) )
+			coord <- NULL
+		if ( !is.character(run) && !is.factor(run) )
+			run <- runNames(object)[run]
+		i_run <- which(run(object) %in% run)
+		index <- intersect(i_run, index)
+	}
+	index
+}
+
+## Basic getters and setters
+
+# note: we follow vector arrangement (length is # of elements/spectra)
+setMethod("length", "SpectralImagingArrays", function(x) nrow(pixelData(x)))
+
+setMethod("names", "SpectralImagingArrays",
+	function(x) rownames(pixelData(x)))
+setReplaceMethod("names", "SpectralImagingArrays",
+	function(x, value) {
+		rownames(pixelData(x)) <- value
+		x
+	})
+
+## Vector-like subsetting
+
+.subset_SpectralImagingArrays <- function(x, i)
+{
+	if ( is.character(i) || is.factor(i) )
+		i <- match(i, pixelNames(x))
+	x@spectraData <- x@spectraData[i,,drop=FALSE]
+	x@elementMetadata <- x@elementMetadata[i,,drop=FALSE]
+	if ( validObject(x) )
+		x
+}
+
+setMethod("[", "SpectralImagingArrays",
+	function(x, i, j, ..., drop = TRUE) {
+		if ( !missing(drop) && isTRUE(drop) )
+			.Warn("'drop' ignored when subsetting ", class(x))
+		.subset_SpectralImagingArrays(x, i)
+	})
+
+setMethod("subset", "SpectralImagingArrays",
+	function(x, subset, ...)
+	{
+		pdata <- as.env(pixelData(x), enclos=parent.frame(2))
+		if ( !missing(subset) ) {
+			i <- eval(substitute(subset), envir=pdata)
+			if ( !is.logical(i) && !is.numeric(i) )
+				.Error("'subset' must specify logical or numeric indices")
+		}
+		if ( missing(subset) ) {
+			x
+		} else {
+			x[i]
+		}
+	})
+
+
+## combine
+
+.combine_SpectralImagingArrays <- function(objects)
+{
+	spectraData <- do.call(c, lapply(objects, spectraData))
+	pixelData <- do.call(rbind, lapply(objects, pixelData))
+	metadata <- do.call(c, lapply(objects, metadata))
+	new(class(objects[[1L]]),
+		spectraData=spectraData,
+		elementMetadata=pixelData,
+		metadata=metadata,
+		processing=list())
+}
+
+setMethod("c", "SpectralImagingArrays",
+	function(x, ...) .combine_SpectralImagingArrays(list(x, ...)))
+
